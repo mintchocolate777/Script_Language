df7ee535522156ffa3489034aa14a8c061d37db0
wangjh79 <wangjh79@naver.com>
2019-05-27 00:07:44 +0900

    검색날짜로 XML불러오기

--- OpenApiParsing.py
+++ OpenApiParsing.py
@@ -1,6 +1,8 @@
+# -*- coding:cp949 -*-
 import urllib
 import http.client
 from xml.dom.minidom import *
+from xml.etree import ElementTree
 
 #http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?
 # #serviceKey=QNsNyJUh2SBMrJ6%2BBGKW54UWg1l3DmN0l0%2F7DjXC%2BLSrzbdKZaHHODRMXS1CQvallUQqH5032TefPXykbUq%2BTQ%3D%3D
@@ -12,8 +14,11 @@ from xml.dom.minidom import *
 # &numOfRows=10
 # &neuter_yn=Y
 
+bgnde = input("검색날짜 시작일을 입력하세요 (yyyymmdd)")
+endde = input("검색날짜 종료일을 입력하세요 (yyyymmdd)")
+
 server = "/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?serviceKey=QNsNyJUh2SBMrJ6%2BBGKW54UWg1l3DmN0l0%2F7DjXC%2BLSrzbdKZaHHODRMXS1CQvallUQqH5032TefPXykbUq%2BTQ%3D%3D"
-params = "&neuter_yn=Y"
+params = "&bgnde=" + bgnde + "&endde=" + endde
 
 conn = http.client.HTTPConnection("openapi.animal.go.kr")
 conn.request("GET",server + params)
@@ -26,5 +31,6 @@ else:
     print (res.read().decode('utf-8'))
 
 conn.close()
+
 
 print(XmlString)
