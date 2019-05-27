8bcf8cc305cf2a0dd7448e11dd65398c7927204f
wangjh79 <wangjh79@naver.com>
2019-05-27 11:51:18 +0900

    삭제

--- xml.py
+++
@@ -1,8 +0,0 @@
-import urllib
-import http.client
-
-conn = http.client.HTTPConnection("openapi.animal.go.kr:80")
-conn.request("GET","/openapi/service/rest/abandonmentPublicSrvc?_wadl&type=xml")
-req = conn.getresponse()
-print(req.status,req.reason)
-print(req.read().decode('utf-8'))
