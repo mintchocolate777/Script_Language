3b050db6102e4bcd14fd32ef2e8afa781c95e1b6
mintchocolate777 <34495962+mintchocolate777@users.noreply.github.com>
2019-06-16 17:39:05 +0900

    배포를 위한 py

---
+++ setup.py
@@ -0,0 +1,8 @@
+
+from distutils.core import setup, Extension
+setup(name='WarmHeart',
+version='1.0',
+py_modules=['gmail', 'mysmtplib', 'noti','OpenApiParsing','OpenSidoApi','OpenSigunguApi','teller','tkinterGUI'],
+data_files=['Image\EmptyHeartButton.png','Image\GraphButton.png','Image\ImageButton.png','Image\MailButton.png','Image\RedHeartButton.png','Image\WarmHeartLogo.png']
+)
+
